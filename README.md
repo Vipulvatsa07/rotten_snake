8a6a404fc5beb1fae1b599c0c43df7f55778ed59
Vipulvatsa07 <vatsavipul.8@gmail.com>
2023-02-21 06:18:40 +0530

    add

---
+++ README.md
@@ -0,0 +1,128 @@
+# “Reliance Digital” Website Clone
+
+Reliance Digital is an Indian consumer electronics retailer. It is a subsidiary of Reliance Retail, a wholly owned subsidiary of Reliance Industries. Reliance Digital opened its first store on 24 April 2007 in Delhi.
+
+![image](https://user-images.githubusercontent.com/25353461/160546076-7073adaa-f343-4d68-8668-fadb3d2f3550.png)
+
+# Reliance-Digital
+### An E-Commerce application which provides the world class products to the consumers.
+
+[![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](https://www.reliancedigital.in/)
+
+
+This is a clone of Reliance Digital web app comprising of it's major features
+
+- Developed for the masses
+- Best latest  products in the market
+- ✨Most competitive lowest prices in the market✨
+
+## Features
+
+- Login / sign Up Page
+- Landing Page
+- Cart Page
+- Products Page
+- Product Description Page
+
+
+## Tech
+
+Reliance Digital uses a number of open source technologies to work properly:
+
+- [HTML] - HTML enhanced for web apps!
+- [VS Code Editor] - awesome web-based text editor
+- [Splide] - Javscript Library for implementing Sliders.
+- [CSS] - Used it for styling the web page.
+- [BootStrap] - Used to implement few designs.
+- [JavaScript] - Used it for the backend implementation.
+
+
+And of course Splide itself is open source library
+ on GitHub.
+
+## Installation
+
+This app requires [VS Code Editor](https://developer.mozilla.org/en-US/) to run.
+
+Do not Install any dependencies and start the contributing to it.
+
+
+## Accessibility
+
+Reliance Digital is currently extended with the following browswers & devices.
+Instructions on how to use them in your own application are linked below.
+
+## Development
+
+Want to contribute? Great!
+
+Reliance Digital uses HTML/CSS/JS + Splide & Bootstrap name for fast developing.
+Make a change in your file and instantaneously see your updates!
+
+Verify the deployment by navigating to your server address in
+your preferred browser.
+
+```sh
+127.0.0.1:5500
+```
+
+## License
+
+MIT
+
+**Free Software, Hell Yeah!**
+
+[//]: # (These are reference links used in the body of this note and get stripped out when the markdown processor does its job. There is no need to format nicely because it shouldn't be seen. Thanks SO - http://stackoverflow.com/questions/4823468/store-comments-in-markdown-syntax)
+
+   [refernce 1]: <reference url>
+   [git-repo-url]: <git repo url>
+   [other useful resources]: <Corresponding url>
+   [markdown-it]: <https://github.com/markdown-it/markdown-it>
+   [VS Code Editor]: <VS Code url>
+   [Javascript]: <https://developer.mozilla.org/en-US/>
+   [Twitter Bootstrap]: <http://twitter.github.com/bootstrap/>
+   [Live Server]: <Live server url>
+
+
+Project Demo
+
+### Landing Page
+
+![image](https://user-images.githubusercontent.com/25353461/161503713-cfe0346f-26b6-465a-821f-6a1bc9e673b3.png)
+
+
+### Product Slider
+
+![image](https://user-images.githubusercontent.com/25353461/161503845-338f4256-91d4-486a-bf09-10ee3be655e5.png)
+
+
+### Products sliders based on categories
+
+![image](https://user-images.githubusercontent.com/25353461/161504024-f2b9b790-2297-457d-9026-17134637ec92.png)
+
+
+### Product Description Page
+
+![image](https://user-images.githubusercontent.com/25353461/161504175-6794e977-2127-491f-9f84-bdd0b6226a95.png)
+
+
+### Cart Page
+
+![image](https://user-images.githubusercontent.com/25353461/161504427-e2f6c26d-998a-4a25-84db-ed82fb24504b.png)
+
+
+### Login Page
+
+![image](https://user-images.githubusercontent.com/25353461/161504506-8ed252b4-759f-4cc3-8fda-2182b679e99c.png)
+
+### Register Page
+
+![image](https://user-images.githubusercontent.com/25353461/161504988-fa619a30-fae9-46a6-8726-6f07178c9fd3.png)
+
+
+### Footer
+
+![image](https://user-images.githubusercontent.com/25353461/161504823-59ab3861-db40-4ffe-830c-967c641d807c.png)
+
+
+
---
+++ main.js
@@ -0,0 +1,260 @@
+// import Splide from '@splidejs/splide';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const slideImage = document.querySelectorAll(".slide-image");
+const slidesContainer = document.querySelector(".slides-container");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const navigationDots = document.querySelector(".navigation-dots");
+
+let numberOfImages = slideImage.length;
+let slideWidth = slideImage[0].clientWidth;
+let currentSlide = 0;
+
+// Set up the slider
+
+function init() {
+  /*   
+    slideImage[0] = 0
+    slideImage[1] = 100%
+    slideImage[2] = 200%
+    */
+
+  slideImage.forEach((img, i) => {
+    img.style.left = i * 100 + "%";
+    // console.log(img);
+  });
+
+  slideImage[0].classList.add("active");
+
+  createNavigationDots();
+
+  //   Creating the automatic Sliding property
+
+  setInterval( () => {
+    if (currentSlide >= numberOfImages - 1) {
+      goToSlide(0);
+      return;
+    }
+  
+    currentSlide++;
+    goToSlide(currentSlide);
+  }, 2200);
+}
+
+init();
+
+// Create navigation dots
+
+function createNavigationDots() {
+  for (let i = 0; i < numberOfImages; i++) {
+    const dot = document.createElement("div");
+    dot.classList.add("single-dot");
+    navigationDots.appendChild(dot);
+
+    dot.addEventListener("click", () => {
+      goToSlide(i);
+    });
+  }
+
+  navigationDots.children[0].classList.add("active");
+}
+
+// Next Button
+
+nextBtn.addEventListener("click", () => {
+  if (currentSlide >= numberOfImages - 1) {
+    goToSlide(0);
+    return;
+  }
+
+  currentSlide++;
+  goToSlide(currentSlide);
+});
+
+// Previous Button
+
+prevBtn.addEventListener("click", () => {
+  if (currentSlide <= 0) {
+    goToSlide(numberOfImages - 1);
+    return;
+  }
+
+  currentSlide--;
+  goToSlide(currentSlide);
+});
+
+// Go To Slide
+
+function goToSlide(slideNumber) {
+  slidesContainer.style.transform =
+    "translateX(-" + slideWidth * slideNumber + "px)";
+
+  currentSlide = slideNumber;
+
+  setActiveClass();
+}
+
+// Set Active Class
+
+function setActiveClass() {
+  // Set active class for Slide Image
+
+  let currentActive = document.querySelector(".slide-image.active");
+  currentActive.classList.remove("active");
+  slideImage[currentSlide].classList.add("active");
+
+  //   set active class for navigation dots
+
+  let currentDot = document.querySelector(".single-dot.active");
+  currentDot.classList.remove("active");
+  navigationDots.children[currentSlide].classList.add("active");
+}
+
+// 5th Integration Slider
+
+
+let flag=0;
+let range=5;
+
+function controller(x){
+    flag = flag+x;
+    slideshow(flag);
+}
+
+
+
+slideshow(flag);
+
+function slideshow(num){
+   let slides=document.getElementsByClassName("slide"); 
+
+if(num==slides.length){
+    flag=0;
+    num=0;
+}
+if(num < 0){
+    flag=slides.length-1;
+    num=slides.length-1;
+}
+
+for(let y of slides){
+    y.style.display="none"
+    y.style.order=0;
+}
+console.log(num)
+
+for(let i=num;i<num+range;i++){
+    let j=i%slides.length
+    slides[j].style.display="block";
+    slides[j].style.order=i;
+}
+   
+}
+
+
+
+// Integration Watch
+
+
+
+// Integration Digital Brand 3 promise
+
+
+
+// Tesing........................................................
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide", {
+            type   : 'loop',
+            autoScroll: {
+              speed: 1,
+            },
+            autoStart: true,
+            rewind: true,
+            pauseOnHover: true,
+        }).mount();
+});
+
+
+// Toshiba
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var splide = new Splide("#check", {
+      type   : 'loop',
+      height     : '35rem',
+      perPage: 4,
+      rewind: true,
+    });
+
+    splide.mount();
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var splide = new Splide("#fat", {
+      type   : 'loop',
+      height     : '35rem',
+      perPage: 4,
+      rewind: true,
+    });
+
+    splide.mount();
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var splide = new Splide("#best", {
+      type   : 'loop',
+      height     : '35rem',
+      perPage: 4,
+      rewind: true,
+    });
+
+    splide.mount();
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var splide = new Splide("#wash", {
+      type   : 'loop',
+      height     : '35rem',
+      perPage: 4,
+      rewind: true,
+    });
+
+    splide.mount();
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var splide = new Splide("#laptop", {
+      type   : 'loop',
+      height     : '35rem',
+      perPage: 4,
+      rewind: true,
+    });
+
+    splide.mount();
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var splide = new Splide("#speakers", {
+      type   : 'loop',
+      height     : '35rem',
+      perPage: 4,
+      rewind: true,
+    });
+
+    splide.mount();
+  });
